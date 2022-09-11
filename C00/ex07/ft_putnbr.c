f07010e6499e4b3faff6c9117137159661862d19
Raj Phuyal <rphuyal@c1r1s6.42lisboa.com>
2022-09-12 00:06:49 +0100

    ex07 done

--- C00/ex07/ft_putnbr.c
+++ C00/ex07/ft_putnbr.c
@@ -6,44 +6,47 @@
 /*   By: rphuyal <rphuyal@student.42lisboa.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/11 15:18:47 by rphuyal           #+#    #+#             */
-/*   Updated: 2022/09/11 16:55:23 by rphuyal          ###   ########.fr       */
+/*   Updated: 2022/09/12 00:05:32 by rphuyal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdio.h>
 
-int	get_num_length(int num)
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int	reverse_number(int num)
 {
 	int	i;
 
 	i = 0;
 	while (num > 0)
 	{
-		x = num % 10;
-		num = nb / 10;
-		i++;
+		i = i * 10;
+		i = i + num % 10;
+		num = num / 10;
 	}
 	return (i);
 }
 
 void	ft_putnbr(int nb)
 {
-	char	x;
-	int	i;
+	int	rev_num;
 
-	i = 0;
-	while (num > 0)
+	rev_num = reverse_number(nb);
+	while(rev_num > 0)
 	{
-		x = num % 10;
-		write(1, )
-		num = nb % 10;
-		i++;
+		ft_putchar(rev_num % 10 + 48);
+		rev_num = rev_num / 10;
 	}
-	return (i);
+	ft_putchar('\n');
+	
 }
 
 int	main(void)
 {
-	ft_putnbr(42);
+	ft_putnbr(42224544);
 }
