88ce895a5171e668e9fbb9fb975124e4bbaa5dd9
Raj Phuyal <rajhphuyal44@gmail.com>
2022-09-21 02:38:09 +0100

    21

--- C04/ex03/ft_atoi.c
+++ C04/ex03/ft_atoi.c
@@ -6,90 +6,47 @@
 /*   By: rphuyal <rphuyal@student.42lisboa.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/20 16:46:39 by rphuyal           #+#    #+#             */
-/*   Updated: 2022/09/20 20:13:22 by rphuyal          ###   ########.fr       */
+/*   Updated: 2022/09/21 01:25:59 by rphuyal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
 #include <stdio.h>
-#include <stdlib.h>
 
-void	ft_putchar(char c)
+int	scan_str(char *str)
 {
-	write(1, &c, 1);
-}
+	int	nminus;
 
-int scan_str(char *str)
-{
-	int	no_of_minus;
-
-	no_of_minus = 0;
+	nminus = 0;
 	while (*str)
 	{
 		if (*str == 45)
-			no_of_minus += 1;
+			nminus += 1;
 		str++;
 	}
-	return (no_of_minus);
+	return (nminus);
 }
 
 int	ft_atoi(char *str)
 {
-	int	no_of_minus;
-	int	return_value;
+	int	nminus;
+	int	result;
 
-	return_value = 0;
-	no_of_minus = scan_str(str);
-	while (*str)
+	result = 0;
+	nminus = scan_str(str);
+	while (*str && !(*str >= 48 && *str <= 57))
+		str++;
+	while (*str && (*str >= 48 && *str <= 57))
 	{
-		if (*str >= 48 && *str <= 57)
-			return_value = return_value * 10;
-			return_value += *str - 48;
+		result = (*str - 48) + (result * 10);
 		str++;
 	}
-	if (no_of_minus % 2 != 0)
-		return_value = -return_value;
-	return (return_value);
+	if (nminus % 2 != 0)
+		result *= -1;
+	return (result);
 }
 
-//from here is test -------------------------------
-void	ft_atoi_like_test(char *str)
+/*int	main(void)
 {
-	int buff;
-	int ft_buff;
-
-	buff = atoi(str);
-	ft_buff = ft_atoi(str);
-	if (buff != ft_buff)
-		printf("> KO, invalid int value returned. expected: %d got: %d\n", buff, ft_buff);
-	else
-		printf("> OK. result: %d\n", ft_buff);
-}
-
-void	ft_atoi_unlike_test(char *str, int expected_result)
-{
-	int ft_buff;
-
-	ft_buff = ft_atoi(str);
-	if (ft_buff != expected_result)
-		printf("> KO, invalid int value returned expected: %d got: %d\n", expected_result, ft_buff);
-	else
-		printf("> OK, result: %d\n", ft_buff);
-}
-
-int		main(void)
-{
-	char *str;
-
-	// quando o comportamento e igual ao de atoi
-	str = " \n \t \r \v +1234567asd";
-	ft_atoi_like_test(str);
-	str = " \n \t\f\r \v -1234b67asd";
-	ft_atoi_like_test(str);
-	// quando o comportamento e diferente ao de atoi
-	str = " \n \t \r \v --+-+-+-2147483648asd";
-	ft_atoi_unlike_test(str, -2147483648);
-	str = " \n \t\f\r \v -++-+-++1234b67asd";
-	ft_atoi_unlike_test(str, -1234);
-	return (0);
-}
+	char	str[] = " \t\v--+--+---12345abcd";
+	printf("%i", ft_atoi(str));
+}*/
